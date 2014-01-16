0cacc4ae2e6d5082062008119ad7fe369d992a7a
MengZhang <meng.zhang15@ufl.edu>
2014-01-16 12:00:11 -0600

    update the source path handling in the test class

--- translator-wofost/src/test/java/org/agmip/translators/wofost/TranslationTest.java
+++ translator-wofost/src/test/java/org/agmip/translators/wofost/TranslationTest.java
@@ -2,9 +2,11 @@ package org.agmip.translators.wofost;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.Map;
 
 import org.agmip.util.JSONAdapter;
@@ -45,7 +47,7 @@ public class TranslationTest
     {
         System.out.println("start");
         
-        FileInputStream fstream = new FileInputStream("src\\test\\resources\\mach_fast.json");
+        FileInputStream fstream = new FileInputStream(this.getClass().getResource("/mach_fast.json").getPath());
         DataInputStream in = new DataInputStream(fstream);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		String json = br.readLine();
@@ -53,7 +55,7 @@ public class TranslationTest
 		Map <String, Object> theMap = JSONAdapter.fromJSON(json);
         
 		WofostOutputController wc = new WofostOutputController();
-		String outputPath = "src\\test\\resources\\OUTPUT";
+		String outputPath = new File(this.getClass().getResource("/OUTPUT/ACMO_meta.dat").getFile()).getParent();
 		wc.writeFiles(outputPath, theMap);
 		
 		System.out.println("finished");
@@ -69,8 +71,8 @@ public class TranslationTest
     	System.out.println("start ACMO");
     	   	
     	WofostACMO wo = new WofostACMO();
-    	String sourceFolder = "src\\test\\resources\\OUTPUT";
-    	String destFolder   = "src\\test\\resources\\OUTPUT";
+    	String sourceFolder = new File(this.getClass().getResource("/OUTPUT/ACMO_meta.dat").getFile()).getParent();
+    	String destFolder   = sourceFolder;
     	
     	wo.execute(sourceFolder, destFolder);
     	
